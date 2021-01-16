9fe3c0007a2c83d6a1bd3e71e28b17877feaca22
April Mulvey <april.mulvey@gmail.com>
2021-01-16 14:42:52 -0800

    Started initial skeleton of page and components. Added styles file

--- src/App.js
+++ src/App.js
@@ -1,36 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import '../src/styles.css';
 import MovieSearch from './components/MovieSearch';
 import { getData } from './network';
 import { useState } from 'react';
-// import NominationsList from './components/NominationsList';
 import ResultsList from './components/ResultsList';
+import NominationsList from './components/NominationsList';
 
 export default function App() {
-  const [movies, setMovies] = useState(null);
-  
+  const [movies, setMovies] = useState([]);
+  const [nominationsList, setNominationsList] = useState([]);
+
+
   const onSubmit = searchTerm => {
    getData(searchTerm).then(data => {
-     
     const result = data.Search
-     console.log("DATA",result)
      setMovies(result);
    })
   }
-  function nominateMovie() {
-    console.log('NOMINATE ME!');
+
+  const addToNominationList = nomination => {
+    nomination.nominated = true;
+    const movie = {...nomination, nominated: true}
+    setMovies(movies.map(m => m.imdbID === nomination.imdbID ? movie : m))
+    console.log('ADD TO NOMINATION LIST', nomination);
+    setNominationsList([...nominationsList, movie])
   }
 
   return (
-    <div>
-      <p onClick={getData}></p>
-      <MovieSearch onSubmit={onSubmit}></MovieSearch>
-
-      <h1>HI</h1>
-    <ResultsList movies={movies} clickMe={nominateMovie}></ResultsList>
-    </div>
+  <body>
+    <section id="section-one">
+      <article className="display-flex">
+        <div className="row">
+              <h1>Shopify Shoppies Movie Search Bar Extravaganza</h1>
+            </div>
+            <div className="row">
+              <p onClick={getData}></p>
+            <MovieSearch onSubmit={onSubmit}></MovieSearch>
+        </div>
+      </article>
+    </section>
+    <section id="section-two">
+      <article className="display-flex">
+        <div className="row">
+              <h1>After Search Results</h1>
+            </div>
+            <div className="row">
+            <ResultsList addToNominationList={addToNominationList} movies={movies}></ResultsList>
+        </div>
+      </article>
+    </section>
+    <section id="section-three">
+      <article className="display-flex">
+        <div className="row">
+              <h1>Nominations List</h1>
+            </div>
+            <div className="row">
+            <NominationsList nominationsList={nominationsList}></NominationsList> 
+        </div>
+      </article>
+    </section>
+  </body>
   );
-
 }
 
 
