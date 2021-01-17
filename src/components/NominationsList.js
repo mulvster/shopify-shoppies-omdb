96d5575876f2427a534e691937ebb79f0c74638d
April Mulvey <april.mulvey@gmail.com>
2021-01-16 20:52:47 -0800

    Move ul tag outside of loop

--- src/components/NominationsList.js
+++ src/components/NominationsList.js
@@ -3,17 +3,34 @@ import { useState ,useCallback } from 'react';
 import ResultsList from './ResultsList';
 
 export default function NominationsList(props) {
-
-  console.log("NOMINATIONS LIST", props.nominationsList)
-  
   
   return (
-    <div>{props.nominationsList.map(movie =>
-      <div>A nomination</div>
-    )
-    
-    }
-    </div>
+    <div className="nomination-list">
+    <ul class="cards">
+      {
+        !!props.movies && props.movies.map((movie, index) => {
+          const movieTitle = movie.Title;
+          const movieid = movie.imdbID;
 
+          const movieReleaseDate = new Date(movie.Released).toDateString();
+
+          return (
+          
+            <li class="cards__item" key={movieid} >
+              <div className="card">
+                <div className="card__content movie" internalnumber={index} movieid={movieid}>
+                  <div className="card__title">
+                    <h2 className="medium-font">{movie.Title}</h2>
+                  </div>
+                  <button className="card-button" onClick={() => props.removeNomination(movie)}>Remove</button>
+                </div>
+              </div>
+            </li>
+         
+          );
+        })
+      }
+    </ul>
+    </div>
   )
 }
